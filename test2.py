b3a76ee87a82bd1f391eda1a3e4106d4551b7176
Adam Wasylewski <awasylew@users.noreply.github.com>
2017-10-20 19:36:12 +0200

    first successfull E2E test

--- test2.py
+++ test2.py
@@ -1,42 +1,38 @@
 import datetime
 import requests
+import json
+import unittest
 
-base = 'https://ping-store.herokuapp.com'
+# ta konfiguracja też ze środowiska?
+# base = 'https://ping-store.herokuapp.com'
+base = 'http://localhost:5000'
 
-def prep1():
-    d = requests.delete(base+'/pings')
-    print(d)
+class test1(unittest.TestCase):
+    def prep1(self):
+        print('prep1()')
+        d = requests.delete(base+'/pings')
+        time=datetime.datetime.now().strftime('%Y%m%d%H')
+        self.p1 = { 'id':101, 'time':time+'0101', 'origin':'o-101', 'target':'t-101', 'success':True, 'rtt':101.01}
+        p = requests.post(base+'/pings', data=json.dumps(self.p1), headers={'Content-type':'application/json'})
+        self.p2 = { 'id':102, 'time':time+'0202', 'origin':'o-102', 'target':'t-102', 'success':True, 'rtt':102.02}
+        p = requests.post(base+'/pings', data=json.dumps(self.p2), headers={'Content-type':'application/json'})
 
-    time=datetime.datetime.now().strftime('%Y%m%d%H')
-    # self.p1 = PingResult(id=101, time=self.time+'0101', origin='o-101', target='t-101', success=True, rtt=101)
-    global p1
-    p1 = { 'id':101, 'time':f'{time}', 'origin':'o-101', 'target':'t-101', 'success':'true', 'rtt':101.01}
-    print(p1)
-    p = requests.post(base+'/pings', data=p1, headers={'Content-type':'application/json'})
-    print(p)
+    def test__get_pings_id__existing(self):
+        """test: przykładowa baza danych, wywołanie z id istniejącym, zwrócony wynik wg id"""
+        self.prep1()
 
-    # self.p1d = self.p1.to_dict()
-    # self.p2 = PingResult(id=102, time=self.time+'0202', origin='o-102', target='t-102', success=True, rtt=102)
-    # self.p2d = self.p2.to_dict()
-    # test_session.add(self.p1)
-    # test_session.add(self.p2)
+        r = requests.get(base+'/pings/101')
+        p = r.json()
+        self.assertEqual(p, self.p1)
 
-def test__get_pings_id__existing():
-    """test: przykładowa baza danych, wywołanie z id istniejącym, zwrócony wynik wg id"""
-    # self.prep1()
-    prep1()
+        r = requests.get(base+'/pings/102')
+        p = r.json()
+        self.assertEqual(p, self.p2)
 
-    # r1 = get_pings_id(101)
-    r = requests.get(base+'/pings/101')
-    # self.assertEqual(r1, self.p1)
-    print(r.text)
-    p = r.json()
-    print(p)
-    global p1
-    assert(p==p1)
-
-    # r2 = get_pings_id(102)
-    # self.assertEqual(r2, self.p2)
+        print('OK test__get_pings_id__existing')
 
 print('hello!')
-test__get_pings_id__existing()
+t = test1()
+t.test__get_pings_id__existing()
+t.test__get_pings_id__existing()
+# wywołanie wszystkich testów z unittest zamiast ręcznego? (ale długo będzie trwać)
