3e9896855a83f77141820b5430dd66d0676f0270
Adam Wasylewski <awasylew@users.noreply.github.com>
2017-11-19 08:18:05 +0100

    should not be in repo

--- set1.bat
+++
@@ -1,7 +0,0 @@
-rem STORE_TEST_DB=sqlite:///:memory:
-set STORE_TEST_DB=sqlite:///pings.db
-rem set STORE_TEST_DB=mysql://pingstore:pingstore@localhost/pingstoretest
-
-rem set STORE_DB=sqlite:///:memory:
-set STORE_DB=sqlite:///pings.db
-rem set STORE_DB=mysql://pingstore:pingstore@localhost/pingstore1
